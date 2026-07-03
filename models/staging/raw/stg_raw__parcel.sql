189ec2fd1a4709cf679c532a5283b8ddfaf3244e
emre.wit.egitim@gmail.com <emre.wit.egitim@gmail.com>
2026-07-03 07:07:09 +0000

    feat: add stg_parcel table

---
+++ models/staging/raw/stg_raw__parcel.sql
@@ -0,0 +1,25 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'parcel') }}
+
+),
+
+renamed as (
+
+    select
+        parcel_id,
+        parcel_tracking,
+        transporter,
+        priority,
+        PARSE_DATE('%B %d, %Y', Date_purCHase) as date_purchase,
+        PARSE_DATE('%B %d, %Y', Date_sHIpping) as date_shipping,
+        PARSE_DATE('%B %d, %Y', DATE_delivery) as date_delivery,
+        PARSE_DATE('%B %d, %Y', DaTeCANcelled) as date_cancelled
+
+    from source
+
+)
+
+select * from renamed
